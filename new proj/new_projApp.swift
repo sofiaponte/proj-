8b3ecce3b9215f7fe8d898a5dca118a7533d5360
Sofia <pontesofia8sp@gmail.com>
2023-07-14 10:16:58 -0400

    Initial Commit

---
+++ new proj/new_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_projApp.swift
+//  new proj
+//
+//  Created by Sofia Ponte  on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct new_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

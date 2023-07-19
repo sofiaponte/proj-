85351d4968619daa8e3e2e9a9e70a45892d8c954
Sofia <pontesofia8sp@gmail.com>
2023-07-19 11:37:10 -0400

    first home screen

--- new proj/ContentView.swift
+++ new proj/ContentView.swift
@@ -8,16 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var name = ""
+  
+   @State private var textTitle = "Welcome to Track-itt! What's your name?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, project!")
-        }
-        .padding()
-    }
-}
+        
+        
+        
+        ZStack {
+            Color(red: 0.6, green:0.6, blue:0.8)
+                .ignoresSafeArea()
+            
+            
+            VStack {
+                
+                Text(textTitle)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 90.0)
+                
+                Image("app")
+                    .resizable()
+                    .padding(.vertical, 30.0)
+                    .padding(.horizontal, 20.0)
+                    
+                
+                
+                
+                TextField("My name is...", text: $name)
+                    
+                   
+                 
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .fontWeight(.regular)
+                    .border(Color.gray, width: 2)
+                 
+          
+                Button("Submit Name") {
+                                textTitle = "Welcome, \(name)!"
+                }
+                .padding(.bottom, 100.0)
+                .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(red:0.6, green: 0.7, blue: 0.9))
+                   
+                
+                
+                
+            }//vstack
+
+            
+            
+            
+            
+        }//zstack
+        
+        
+    }// some view
+}//content view
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
